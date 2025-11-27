4ff75723b6bc8a58761ac9e04c3c8faefa94e359
R-JDiaz <diazrj54@gmail.com>
2025-11-28 00:11:20 +0800

    final obs in basic

--- openmp/basic.c
+++ openmp/basic.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <omp.h>
 
+
 int main() {
-    
+
+    int sum = 0;
     //Thread
     omp_set_num_threads(8);
     double start = omp_get_wtime();
     #pragma omp parallel
     {
-        printf("Hello from thread \n");
+        sum += 1;
         #pragma omp single 
         {
         printf("Num of Threads: %d \n", omp_get_num_threads());
         }
     }
+    printf("Sum : %d\n", sum);
     
+    sum = 0;
     double end = omp_get_wtime();
     printf(" Parallel time : %.4f\n\n", end - start);
     
     //Loop
-    for (int i = 0; i < 9; i++) {
-        printf("[%d] Hello from for loop \n", i);
+    for (int i = 0; i < 8; i++) {
+        sum += 1;
     }
     double end2 = omp_get_wtime();
     printf("Loop time : %.4f", end2 - end);
-    
+    printf("Sum : %d\n", sum);
     
     return 0;
 }
@@ -35,5 +39,10 @@ int main() {
     [A] PRINT (OUTPUT)
     LOOP WINS MODERATELY
 
-    
+    [B] UPDATING 
+    LOOP WINS MODERATELY 
+
+
+    CONCLUSION: FOR SMALLER TASKS SIMPLE FOR LOOP IS OKAY HOWEVER,
+    FOR HEAVY TASKS THREADS IS RECOMMENDABLE
 */
