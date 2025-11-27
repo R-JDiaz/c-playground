817117499b5e543630e22579112ac940b0cf2d62
R-JDiaz <diazrj54@gmail.com>
2025-11-27 23:31:07 +0800

    exp critical

---
+++ openmp/syncTools.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+    int sum = 0;
+
+    #pragma omp parallel for 
+    for (int 1 = 1; i <= 10; i ++) {
+        #pragma omp critical 
+        {
+            sum += 1;
+            printf("Thread %d added %d, sum = %d\n", omp_get_thread_num(), i, sum);
+        }
+    }
+
+    printf("Total sum = %d\n", sum);
+    return 0;
+}
