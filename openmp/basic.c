1b439703882b20902509e8652781d715482560ad
R-JDiaz <diazrj54@gmail.com>
2025-11-27 23:59:28 +0800

    nums

--- openmp/basic.c
+++ openmp/basic.c
@@ -2,21 +2,24 @@
 #include <omp.h>
 
 int main() {
-    omp_set_num_threads(8);
     
+    //Thread
+    omp_set_num_threads(8);
     double start = omp_get_wtime();
     #pragma omp parallel
     {
         printf("Hello from thread \n");
+        printf("Num of Threads: %d \n", omp_get_num_threads());
     }
-    omp_get_thread_num();
+    
     double end = omp_get_wtime();
-    printf("Parallel time : %.4f\n\n", end - start);
+    printf(" Parallel time : %.4f\n\n", end - start);
 
     
+    //Loop
     double start2 = omp_get_wtime();
     for (int i = 0; i < 9; i++) {
-        printf("Hello from for loop \n");
+        printf("[%d] Hello from for loop \n", i);
     }
     double end2 = omp_get_wtime();
     printf("Loop time : %.4f", end2 - start2);
