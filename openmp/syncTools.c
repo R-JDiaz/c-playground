c33cd1b48915c6064958bcc12ddf54c577ea140a
R-JDiaz <diazrj54@gmail.com>
2025-11-27 23:37:26 +0800

    critical sync visible presentation

--- openmp/syncTools.c
+++ openmp/syncTools.c
@@ -9,10 +9,15 @@ int main() {
         #pragma omp critical 
         {
             sum += 1;
-            printf("Thread %d added %d, sum = %d\n", omp_get_thread_num(), i, sum);
+            printf("Thread %d added %d, sum = %d\n", omp_get_thread_num(), i, sum+1);
         }
+
+        printf("OutSide: Thread %d added %d, sum = %d\n", omp_get_thread_num(), i, sum);
     }
 
     printf("Total sum = %d\n", sum);
     return 0;
 }
+
+/* notes:
+critical - it isolates the process of t */
