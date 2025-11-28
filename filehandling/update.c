445e2be16ad15da59fc18bfb2870c53e37e64961
R-JDiaz <diazrj54@gmail.com>
2025-11-28 21:41:34 +0800

    simple insert

--- filehandling/update.c
+++ filehandling/update.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TEXT_FILE "./update.txt"
+#define TEXT_FILE "./update.dat"
 
 typedef struct Student {
-    int id;
+    char id;
     char name[100];
     char course[4];
 } Student;
+
+typedef struct Header {
+    int id;
+    int index;
+} Header;
+
 
 void insert(FILE *f, Student student) {
     fseek(f, 0, SEEK_END);
     fwrite(&student, sizeof(Student), 1, f);
 }
+
+
 
 int main() {
     FILE *file = fopen(TEXT_FILE, "ab+");
@@ -20,7 +28,7 @@ int main() {
         printf("File failed to open");
         return 1;
     }
-    Student student = {1, "RJ DIAZ", "BSCS"};
+    Student student = {1, "SYRENE DEJINO", "BSED"};
 
     insert(file, student);
     return 0; 
