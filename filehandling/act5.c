aa206d7500618cd2ed62535f0f50d3f7414d0044
R-JDiaz <diazrj54@gmail.com>
2025-11-29 16:35:38 +0800

    insert, update, read . no parallel yet

--- filehandling/act5.c
+++ filehandling/act5.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h> 
 
 #define DAT_FILE "act.dat"
 #define TMP_FILE "act.tmp"
@@ -10,6 +12,61 @@ typedef struct Student{
     char course[5];
 } Student;
 
+void insert(FILE *f, Student *student) {
+    fseek(f, 0, SEEK_END);
+    fwrite(student, sizeof(Student), 1, f);
+    printf("ADDED SUCCESSFULLY\n");
+}
+
+Student get_student(FILE *f, int id) {
+    rewind(f);
+    Student stud;
+    while(fread(&stud, sizeof(Student), 1, f) == 1) {
+        if(stud.id == id) {
+            return stud;
+        }
+    }
+    Student empty = {0};  
+    return empty;
+}
+
+void update(FILE *f, int id, const char *newName, const char *newCourse) {
+    rewind(f);
+    Student stud;
+    long pos;
+
+    while(fread(&stud, sizeof(Student), 1, f) == 1) {
+        if(stud.id == id) {
+            pos = ftell(f) - sizeof(Student);
+
+            int update = 0;
+            if (newName && strcmp(stud.name, newName) != 0) {
+                strcpy(stud.name, newName);
+                update++;
+            } else if (newCourse && strcmp(stud.course, newCourse) != 0)  {
+                strcpy(stud.course, newCourse);
+                update++;
+            }
+
+            if (update) {
+                fseek(f, pos, SEEK_SET);
+                fwrite(&stud, sizeof(Student), 1, f);
+                printf("Updated Succesfully\n"); 
+            } else {
+                printf("Nothing to Update\n");
+            }
+            return;
+        }
+    }
+    printf("Student not found.\n");   
+}
+
+void displayStudent(Student student) {
+    if (student.id == 0) { printf("NOT FOUND\n"); return;}
+    printf("ID : %d\n", student.id);
+    printf("NAME: %s\n", student.name);
+    printf("COURSE: %s\n\n", student.course);
+}
 int main() {
     FILE *file = fopen(DAT_FILE, "rb+");
     if (!file) {
@@ -17,5 +74,14 @@ int main() {
         if (file) { fclose(file); };
     }
 
+    Student stud = {2, "jay", "bscs"};
+    insert(file, &stud);
+    displayStudent(get_student(file, 9));
+    displayStudent(get_student(file, 1));
+    displayStudent(get_student(file, 2));
+    update(file, 2, "RJ", "BSED");
+    displayStudent(get_student(file, 2));
+
+    
     return 0;
 }
